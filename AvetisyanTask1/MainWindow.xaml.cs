db9c2e217dd49e116e878cfc86ec613e460ac22d
GeekHolo <artem.avetis@yande.ru>
2022-01-09 18:43:03 +0300

    Добавил динамически создаваемые текстбоксы

--- AvetisyanTask1/MainWindow.xaml.cs
+++ AvetisyanTask1/MainWindow.xaml.cs
@@ -23,6 +23,40 @@ namespace AvetisyanTask1
         public MainWindow()
         {
             InitializeComponent();
+           
+            string input = indexInput.Text;
+            string[] inputMass = input.Split(',');
+            int numberOfindexes = inputMass.Length;
+            char[] exceptionsForCounting = { '.', ',', '?', '!', '-', '_', '"', ' ', '»', '«', '|' };
+
+            for (var i = 0; i < numberOfindexes; i++)
+                {
+                string convertedString = stringConverter(inputMass[i], exceptionsForCounting);
+
+                    TextBox txtBox = new TextBox();
+                    outputText.Children.Add(txtBox);
+                    txtBox.Text = inputMass[i];
+
+
+                    TextBox txtBoxLetters = new TextBox();
+                    outputLetters.Children.Add(txtBox);
+                    txtBoxLetters.Text = convertedString.Length.ToString();
+
+                    TextBox txtBoxVowels = new TextBox();
+                    outputVowels.Children.Add(txtBox);
+                }
+
+        }
+        
+        public string stringConverter(string str, char[] exceptionsForCounting )
+        {
+            string newStr = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (exceptionsForCounting.Contains(str[i]) != true)
+                    newStr += str[i];             
+            }
+            return newStr;
         }
     }
 }
